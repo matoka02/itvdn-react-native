14e7e8eda7a0f4de04271e4fa63452bf8fc756a7
Anastasiia <120492521+matoka02@users.noreply.github.com>
2024-11-04 20:43:04 +0200

    018 backgroundfetch

---
+++ App.styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+  },
+  title: {
+    display: 'flex',
+    alignSelf: 'center',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 30,
+    marginBottom: 80,
+  },
+});
+
+export default styles;
